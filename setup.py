45049b6eb00dfd7899500bd6a6de7d91c4260cd9
yizzuide <fu837014586@163.com>
2023-11-29 22:44:39 +0800

    feat(yml): Upgrade autoboot for support yml features

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-VERSION = '0.1.2'
+VERSION = '0.2.0'
 
 with open('README.md', 'r', encoding='utf-8') as fp:
     long_description = fp.read()
@@ -18,7 +18,7 @@ setup(
     packages=find_packages(exclude=['tests*']),
     include_package_data=True,
     install_requires=[
-        'autoboot>=0.5.1',
+        'autoboot>=0.6.0',
         'redis'
     ],
     tests_require=[
